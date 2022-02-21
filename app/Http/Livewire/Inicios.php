c5b8e37c2c4870deb3846115f091582962f18ef7
Edmétrio J <edmetrio06@gmail.com>
2022-02-21 14:19:41 +0200

    controller inicio

---
+++ app/Http/Livewire/Inicios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Inicios extends Component
+{
+    public function render()
+    {
+        return view('livewire.inicios')->layout('layouts.template');
+    }
+}
