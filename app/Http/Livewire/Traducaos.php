c9f48350017ebff923661a1ab88abd9cbd3e71fc
Edmétrio J <edmetrio06@gmail.com>
2022-02-21 14:20:16 +0200

    controller traducao

---
+++ app/Http/Livewire/Traducaos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Traducaos extends Component
+{
+    public function render()
+    {
+        return view('livewire.traducaos');
+    }
+}
