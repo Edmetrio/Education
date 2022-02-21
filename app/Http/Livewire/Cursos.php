e4ab2ef3f9028be0b4c64219a0b2b42a578123ff
Edmétrio J <edmetrio06@gmail.com>
2022-02-21 14:17:15 +0200

    controller cursos

---
+++ app/Http/Livewire/Cursos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Cursos extends Component
+{
+    public function render()
+    {
+        return view('livewire.cursos');
+    }
+}
