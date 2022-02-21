8bc0b790565e9aa74283aab8328c158a12157710
Edmétrio J <edmetrio06@gmail.com>
2022-02-21 14:16:22 +0200

    controller aplicars

---
+++ app/Http/Livewire/Aplicars.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Aplicars extends Component
+{
+    public function render()
+    {
+        return view('livewire.aplicars');
+    }
+}
