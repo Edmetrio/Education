82ca4da741496b3ed0558a97cc421a367b8ea784
Edmétrio J <edmetrio06@gmail.com>
2022-02-21 14:16:11 +0200

    controller anos

---
+++ app/Http/Livewire/Anos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Anos extends Component
+{
+    public function render()
+    {
+        return view('livewire.anos');
+    }
+}
