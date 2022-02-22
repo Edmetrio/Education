635369f91e11b8b32e609cbd957b4dd9e3ea0a9b
Edmétrio J <edmetrio06@gmail.com>
2022-02-22 13:26:12 +0200

    bolsas livewire

--- app/Http/Livewire/Bolsas.php
+++ app/Http/Livewire/Bolsas.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Servico;
 use Livewire\Component;
 
 class Bolsas extends Component
 {
+    public function mount()
+    {
+        $sr = Servico::orderBy('created_at', 'desc')->get();
+    }
+
     public function render()
     {
-        return view('livewire.bolsas')->layout('layouts.app');;
+        $this->servico = Servico::orderBy('created_at', 'desc')->get();
+        $sr = Servico::orderBy('created_at', 'desc')->get();
+        return view('livewire.bolsas')->layout('layouts.app', compact('sr'));;
     }
 }
