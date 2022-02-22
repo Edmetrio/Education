9e6d59115a797e437abfeca97e68430ec646d62e
Edmétrio J <edmetrio06@gmail.com>
2022-02-22 13:26:35 +0200

    inicios livewire

--- app/Http/Livewire/Inicios.php
+++ app/Http/Livewire/Inicios.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Servico;
 use Livewire\Component;
 
 class Inicios extends Component
 {
     public function render()
     {
+        $this->servico = Servico::orderBy('created_at', 'desc')->get();
         return view('livewire.inicios')->layout('layouts.template');
     }
 }
