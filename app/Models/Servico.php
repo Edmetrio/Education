3f8d5045294d0ef0306baec2110da5d99fdf82aa
Edmétrio J <edmetrio06@gmail.com>
2022-02-22 13:26:54 +0200

    model servico

--- app/Models/Servico.php
+++ app/Models/Servico.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Servico extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'servico';
+    protected $fillable = ['nome','icon','descricao','texto','estado'];
 }
