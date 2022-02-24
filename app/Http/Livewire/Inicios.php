16f2b2254f532538d8b890c5ade8fba301b55b76
Edmétrio J <edmetrio06@gmail.com>
2022-02-24 08:33:28 +0200

    array depoimento, parceiro

--- app/Http/Livewire/Inicios.php
+++ app/Http/Livewire/Inicios.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Models\Depoimento;
+use App\Models\Models\Parceiro;
 use App\Models\Servico;
 use Livewire\Component;
 
@@ -10,6 +12,8 @@ class Inicios extends Component
     public function render()
     {
         $this->servico = Servico::orderBy('created_at', 'desc')->get();
+        $this->depoimento = Depoimento::orderBy('created_at', 'desc')->get();
+        $this->parceiro = Parceiro::orderBy('created_at', 'desc')->get();
         return view('livewire.inicios')->layout('layouts.template');
     }
 }
