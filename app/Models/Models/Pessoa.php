3abb4896a09f313cd5fa828ad0bc8d4167effbf5
Edmétrio J <edmetrio06@gmail.com>
2022-02-24 08:35:10 +0200

    Model Pessoa

---
+++ app/Models/Models/Pessoa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Pessoa extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'pessoa';
+    protected $fillable = ['sexo_id','telefone','nome','nascimento','passaporte','email'];
+}
