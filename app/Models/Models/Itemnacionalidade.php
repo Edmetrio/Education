b1907f232332a421643ef13d14bbc76e8e6d5b5d
Edmétrio J <edmetrio06@gmail.com>
2022-02-24 08:34:42 +0200

    Model Itemnacionalidade

---
+++ app/Models/Models/Itemnacionalidade.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+
+class Itemnacionalidade extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'itemnacionalidade';
+    protected $fillable = ['pessoa_id','nacionalidade_id'];
+}
