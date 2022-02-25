cb2985af6f91669f44d2a6a1e9eae50621ccc3be
Edmétrio J <edmetrio06@gmail.com>
2022-02-25 10:13:00 +0200

    model sexo

---
+++ app/Models/Models/Sexo.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Sexo extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'sexo';
+    protected $fillable = ['nome'];
+}
