f19671ffd9710005299d0045381f096b175ec73d
Edmétrio J <edmetrio06@gmail.com>
2022-02-25 10:12:53 +0200

    model poost

---
+++ app/Models/Models/Poost.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+
+class Poost extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'poost';
+    protected $fillable = ['nome','icon','descricao'];
+}
