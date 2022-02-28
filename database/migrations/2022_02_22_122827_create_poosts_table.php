9c65bdba279dbfea8e35b96baa30d7cb0962c5f8
Edmétrio J <edmetrio06@gmail.com>
2022-02-28 16:05:16 +0200

    model poosts

---
+++ database/migrations/2022_02_22_122827_create_poosts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePoostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poost', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome')->unique();
+            $table->string('descricao')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poost');
+    }
+}
