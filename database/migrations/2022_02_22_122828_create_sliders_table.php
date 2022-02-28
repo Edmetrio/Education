0f44c56dd587732ad07ba19319a886371227a9d6
Edmétrio J <edmetrio06@gmail.com>
2022-02-28 16:05:32 +0200

    model sliders

---
+++ database/migrations/2022_02_22_122828_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slider', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome')->unique();
+            $table->longText('descricao')->nullable();
+            $table->longText('texto')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slider');
+    }
+}
