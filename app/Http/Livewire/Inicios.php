8c1e2f45a6a75421b83f0288a209c17791ec9d08
Edmétrio J <edmetrio06@gmail.com>
2022-02-28 16:03:14 +0200

    add array depoimento

--- app/Http/Livewire/Inicios.php
+++ app/Http/Livewire/Inicios.php
@@ -11,12 +11,13 @@ use Livewire\Component;
 
 class Inicios extends Component
 {
+
     public function render()
     {
         $poost = Poost::orderBy('created_at', 'desc')->paginate(4);
         $this->servico = Servico::orderBy('created_at', 'desc')->get();
         $sr = Servico::orderBy('created_at', 'desc')->get();
-        $depoimento = Depoimento::orderBy('created_at', 'desc')->paginate(4);
+        $depoimento = Depoimento::orderBy('created_at', 'desc')->paginate(6);
         $parceiro = Parceiro::orderBy('created_at', 'desc')->paginate(4);
         $slider = Slider::orderBy('created_at', 'desc')->paginate(6);
         return view('livewire.inicios', compact('poost','parceiro','depoimento'))->layout('layouts.template', compact('slider','sr'));
