c8b9e44549abd0531074115508cfc0fc4b53d01d
Edmétrio J <edmetrio06@gmail.com>
2022-02-28 16:03:02 +0200

    add mount function

--- app/Http/Livewire/DetalheServico.php
+++ app/Http/Livewire/DetalheServico.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Models\Pacoteinscricao;
+use App\Models\Models\Poost;
 use App\Models\Servico;
 use Livewire\Component;
 
@@ -11,11 +13,14 @@ class DetalheServico extends Component
 
     public function mount($id)
     {
-        $this->servico = Servico::findorFail($id);
+        $this->servico = Servico::with('pacotes')->findorFail($id);
+        $this->servicos = Servico::orderBy('created_at', 'desc')->get();
     }
     public function render()
     {
+        $this->inscricao = Pacoteinscricao::orderBy('created_at', 'desc')->get();
         $sr = Servico::orderBy('created_at', 'desc')->get();
-        return view('livewire.detalhe-servico')->layout('layouts.app', compact('sr'));
+        $poost = Poost::orderBy('created_at', 'desc')->paginate(4);
+        return view('livewire.detalhe-servico', compact('poost'))->layout('layouts.app', compact('sr'));
     }
 }
