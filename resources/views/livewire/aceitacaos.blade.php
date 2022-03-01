8656df54e9d206f1e6bd7ab7ca4cee11ba17b444
Edmétrio J <edmetrio06@gmail.com>
2022-03-01 18:09:34 +0200

    modal aceitacao

---
+++ resources/views/livewire/aceitacaos.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="AceitacaoModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="staticBackdropLabel" >Aceitaçao</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <h3 class="mb-3">Exemplo de Aceitação</h3>
+            <div class="col-md-11 col-sm-12">
+                <div class="about-img">
+                    <img src="img/site/Aceitaçao.jpg" alt="about">
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Descordo</button> -->
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>
+            </div>
+        </div>
+    </div>
+</div>
