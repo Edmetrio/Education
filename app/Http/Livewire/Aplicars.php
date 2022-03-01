3175bd4abe830e04d31479f024e5ea69ac92d131
Edmétrio J <edmetrio06@gmail.com>
2022-03-01 18:05:20 +0200

    store apply

--- app/Http/Livewire/Aplicars.php
+++ app/Http/Livewire/Aplicars.php
@@ -2,11 +2,47 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\Teste;
+use App\Models\Models\Poost;
 use App\Models\Servico;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class Aplicars extends Component
 {
+    public $nome, $email, $assunto, $mensagem;
+
+    public function resetInput()
+    {
+        $this->nome = '';
+        $this->email = '';
+        $this->assunto = '';
+        $this->mensagem = '';
+    }
+    
+
+    public function store()
+    {
+        $validatedDate = $this->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'assunto' => 'required',
+            'mensagem' => 'required'
+        ]);
+        $detail = $validatedDate;
+        Mail::to('edmetrio@firsteducation.edu.mz')->send(new Teste($detail));
+        session()->flash('status', 'E-mail Enviado com Sucesso!');
+        $this->resetInput();
+    }
+
+    public function mount()
+    {
+        $this->servicos = Servico::orderBy('created_at', 'desc')->get();
+        $this->poost = Poost::orderBy('created_at', 'desc')->get();
+    }
+
+    
+
     public function render()
     {
         $sr = Servico::orderBy('created_at', 'desc')->get();
