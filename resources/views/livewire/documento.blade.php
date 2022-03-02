cbaa8c443f519113b82b3488a21461c66035e683
Edmétrio J <edmetrio06@gmail.com>
2022-03-02 22:14:26 +0200

    modal documentos adicionais

---
+++ resources/views/livewire/documento.blade.php
@@ -0,0 +1,28 @@
+<div class="modal fade" id="documentoModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="staticBackdropLabel" >Documentos Adicionais</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <h3>Documentos Adicionais</h3>
+                <ol class="col-sm">
+                    <li>1. Certificado traduzido e homologado</li>
+                    <li>2. Carta de Motivação traduzida (Feita por candidato)</li>
+                    <li>3. Duas (2) Cartas de Recomendação traduzidas (Feita por professor)</li>
+                    <li>4. Duas (2) Fotos tipo passe</li>
+                    <li>5. Curriculum Vitae</li>
+                    <li>6. Exame de Proficiência (IELTS, TOEFL, Cambridge, entre outros)</li>
+                </ol>
+            </div>
+            <p><h5>Nota bem:</h5> Os documentos traduzidos é para quem deseja ter aulas com ensino de instrução inglês </p>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Descordo</button> -->
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>
+            </div>
+        </div>
+    </div>
+</div>
