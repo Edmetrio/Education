d7813cf65dad339b2bbb90e132f8b7185d67b64f
Edmétrio J <edmetrio06@gmail.com>
2022-03-02 22:14:56 +0200

    blade perfil

---
+++ resources/views/livewire/perfils.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="PerfilModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="staticBackdropLabel" >Perfil das Universidades</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <h3 class="mb-3">Perfil das Universidades</h3>
+            <div class="col-md-11 col-sm-12">
+                <div class="about-img">
+                    <img src="img/site/Sem.png" alt="about">
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Descordo</button> -->
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>
+            </div>
+        </div>
+    </div>
+</div>
