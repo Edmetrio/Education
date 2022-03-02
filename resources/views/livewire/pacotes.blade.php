ce3aa815777be0e7e1199e91ede6180386fc7a9c
Edmétrio J <edmetrio06@gmail.com>
2022-03-02 22:14:46 +0200

    blade pacotes

---
+++ resources/views/livewire/pacotes.blade.php
@@ -0,0 +1,192 @@
+<div>
+    <div class="banner-area-wrapper">
+        <div class="banner-area text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="banner-content-wrapper">
+                            <div class="banner-content">
+                                <h2>Gerir Pacotes</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Banner Area End -->
+    <!-- Blog Start -->
+    <div class="courses-details-area blog-area pt-150 pb-140">
+        <div class="container">
+            <div class="row">
+                @if($createData == false)
+                <div class="col-md-8 mb-50">
+                    <div class="courses-details">
+                        <div class="course-details-content">
+                            <h2 class="text-center">Pacotes</h2>
+                            @if ($message = Session::get('status'))
+                            <div>
+                                <p class="alert alert-success" class="table p-field p-col-12 p-md-6 table-striped" style="text-align: center;">{{ $message }}</p>
+                            </div>
+                            @endif
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <strong>Opss!</strong> Algum problema com seu formulário<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <hr>
+                            <form>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Serviço:</strong>
+                                            <select class="form-control" wire:model="servico_id">
+                                                <option>Seleccione o Serviço</option>
+                                                @foreach($servico as $s)
+                                                <option value="{{ $s->id }}">{{ $s->nome }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('nome') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Nome:</strong>
+                                            <input type="text" wire:model="nome" class="form-control" placeholder="Nome">
+                                            @error('nome') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Valor:</strong>
+                                            <input type="number" wire:model="valor" class="form-control" placeholder="2000">
+                                            @error('valor') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <button type="submit" wire:click.prevent="store()" class="btn btn-primary">Adicionar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endif
+
+                @if($updateData == true)
+                <div class="col-md-8">
+                    <div class="courses-details">
+                        <div class="course-details-content">
+                            <h2>Poost</h2>
+                            @if ($message = Session::get('status'))
+                            <div>
+                                <p class="alert alert-success" class="table p-field p-col-12 p-md-6 table-striped" style="text-align: center;">{{ $message }}</p>
+                            </div>
+                            @endif
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <strong>Opss!</strong> Algum problema com seu formulário<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <hr>
+                            <form>
+                                <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Serviço:</strong>
+                                            <select class="form-control" wire:model="edit_servico_id">
+                                                <option value="{{ $edit_servico_id }}">{{ $edit_servico_nome }}</option>
+                                                @foreach($servico as $s)
+                                                <option value="{{ $s->id }}">{{ $s->nome }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('nome') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Nome:</strong>
+                                            <input type="text" wire:model="edit_nome" class="form-control" placeholder="Nome">
+                                            @error('edit_nome') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>valor:</strong>
+                                            <input type="text" wire:model="edit_valor" class="form-control" placeholder="2000">
+                                            @error('edit_valor') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <button type="submit" wire:click.prevent="update('{{$edit_id}}')" class="btn btn-primary">Alterar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endif
+
+                <div class="col-md-4">
+                    <div class="blog-sidebar right">
+                        <div class="single-blog-widget mb-50">
+                            <h3>Aplica-se já</h3>
+                            <hr>
+                            <ul>
+                                <li><a href="{{url("inscricao")}}">Inscrições universitárias</a></li>
+                                <li><a href="{{url("bolsa")}}">Bolsas de Estudos</a></li>
+                                <li><a href="{{url("online")}}">Cursos Online</a></li>
+                                <li><a href="{{url("fundacao")}}">Ano de Fundação</a></li>
+                                <li><a href="#">Prdução de CV</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                @if($showData == true)
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Serviço</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Valor</th>
+                                    <th scope="col">Criação</th>
+                                    <th scope="col">Acções</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($pacote as $s)
+                                <tr>
+                                    <td>{{ $s->servicos->nome }}</td>
+                                    <td>{{ $s->nome }}</td>
+                                    <td>{{ $s->valor }}</td>
+                                    <td>{{ $s->created_at->diffForhumans() }}</td>
+                                    <td>
+                                        <button wire:click="edit('{{$s->id}}')"><i class="fas fa-edit"></i></button>
+                                        <button wire:click="delete('{{ $s->id }}')"><i class="fab fa-instagram"></button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                @endif
+                <hr>
+            </div>
+        </div>
+        <!-- Blog End -->
+    </div>
+</div>
