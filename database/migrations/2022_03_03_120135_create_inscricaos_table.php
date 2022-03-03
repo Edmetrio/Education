3d1197040b7cb101e2733b15a300061e83ee696a
Edmétrio J <edmetrio06@gmail.com>
2022-03-03 15:08:13 +0200

    table inscricao

---
+++ database/migrations/2022_03_03_120135_create_inscricaos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInscricaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscricao', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('users_id')->nullable();
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('servico_id')->nullable();
+            $table->foreign('servico_id')->references('id')->on('servico')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('pais_id')->nullable();
+            $table->foreign('pais_id')->references('id')->on('pais')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('intake_id')->nullable();
+            $table->foreign('intake_id')->references('id')->on('intake')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('grau_id')->nullable();
+            $table->foreign('grau_id')->references('id')->on('grau')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('universidade')->nullable();
+            $table->decimal('propina', 20,2)->nullable();
+            $table->string('fecho')->nullable();
+            $table->string('link')->nullable();
+            $table->string('anexo')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscricao');
+    }
+}
