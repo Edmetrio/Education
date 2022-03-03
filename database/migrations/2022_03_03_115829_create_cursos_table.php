e1ff58f80fc60186ff19d27076cd815575ab4aa5
Edmétrio J <edmetrio06@gmail.com>
2022-03-03 15:07:38 +0200

    table entidade

---
+++ database/migrations/2022_03_03_115829_create_cursos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curso', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome')->nullable();
+            $table->uuid('categoria_id')->nullable();
+            $table->foreign('categoria_id')->references('id')->on('categoria')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curso');
+    }
+}
