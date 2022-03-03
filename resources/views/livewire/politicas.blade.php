a00cda3865ae9149154ca0fb641b89f5729acc40
Edmétrio J <edmetrio06@gmail.com>
2022-03-03 15:06:28 +0200

    blade politicas

---
+++ resources/views/livewire/politicas.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="addStudentModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="staticBackdropLabel" >Requisitos de Aplicação</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <h3>Requisitos Principais</h3>
+                <ol class="col-sm">
+                    <li>1. Uma Cópia de Passaporte Válida/ Bilhete de Identificação (BI)</li>
+                    <li>2. Certificado/Diploma/Declaração de Notas</li>
+                    <li>3. Talão de Depósito</li>
+                </ol>
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Descordo</button> -->
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>
+            </div>
+        </div>
+    </div>
+</div>
