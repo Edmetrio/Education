9034d9b0433ef1167a18c67be3818eae579f8f83
Edmétrio J <edmetrio06@gmail.com>
2022-03-04 16:28:14 +0200

    Modal Curso

---
+++ app/Models/Models/Curso.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Curso extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'curso';
+    protected $fillable = ['nome','categoria_id','estado'];
+
+    public function categorias()
+    {
+        return $this->hasOne(Categoria::class, 'id', 'categoria_id');
+    }
+}
