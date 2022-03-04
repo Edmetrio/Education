9993378f2c7827ca7d742ab65ebd365adf45ba65
Edmétrio J <edmetrio06@gmail.com>
2022-03-04 16:29:21 +0200

    Modal Itemintake

---
+++ app/Models/Models/Itemintake.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Itemintake extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'itemintake';
+    protected $fillable = ['inscricao_id','intake_id','estado'];
+}
