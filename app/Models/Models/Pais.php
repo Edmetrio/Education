7ab307ef61f1f5e2c39f0883bb5cd2db417f9f75
Edmétrio J <edmetrio06@gmail.com>
2022-03-07 08:40:06 +0200

    model pais

--- app/Models/Models/Pais.php
+++ app/Models/Models/Pais.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Pais extends Model
+class Pais extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'pais';
+    protected $fillable = ['nome'];
+
+    public function Inscricaos()
+    {
+        return $this->hasMany(Inscricao::class, 'entidade_id');
+    }
 }
