e39b06dbb610befdd9e73a93150c5b299addbb37
Edmétrio J <edmetrio06@gmail.com>
2022-03-07 08:39:54 +0200

    model intake

--- app/Models/Models/Intake.php
+++ app/Models/Models/Intake.php
@@ -4,8 +4,16 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Intake extends Model
+class Intake extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'intake';
+    protected $fillable = ['nome','estado'];
 }
