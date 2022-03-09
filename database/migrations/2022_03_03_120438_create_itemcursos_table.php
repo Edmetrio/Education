ec59225d466707ef9275c3a8478a2044edb0737b
Edmétrio J <edmetrio06@gmail.com>
2022-03-09 11:19:57 +0200

    model itemcurso

--- database/migrations/2022_03_03_120438_create_itemcursos_table.php
+++ database/migrations/2022_03_03_120438_create_itemcursos_table.php
@@ -17,8 +17,17 @@ class CreateItemcursosTable extends Migration
             $table->uuid('id')->primary();
             $table->uuid('inscricao_id')->nullable();
             $table->foreign('inscricao_id')->references('id')->on('inscricao')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('servico_id')->nullable();
+            $table->foreign('servico_id')->references('id')->on('servico')->onDelete('cascade')->onUpdate('cascade');
             $table->uuid('curso_id')->nullable();
             $table->foreign('curso_id')->references('id')->on('curso')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('grau_id')->nullable();    
+            $table->foreign('grau_id')->references('id')->on('grau')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('entidade_id')->nullable();
+            $table->foreign('entidade_id')->references('id')->on('entidade')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('moeda_id')->nullable();
+            $table->foreign('moeda_id')->references('id')->on('moeda')->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('propina', 20,2)->nullable();
             $table->string('estado')->default('on')->nullable();
             $table->timestamps();
         });
