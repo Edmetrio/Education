44ce4757e99438076b9a0d24a0c8b061e5347ca5
Edmétrio J <edmetrio06@gmail.com>
2022-03-15 15:20:04 +0200

    blade teacher

--- resources/views/teacher.blade.php
+++ resources/views/teacher.blade.php
@@ -4,131 +4,131 @@
 
 <!-- Banner Area Start -->
 <div class="banner-area-wrapper">
-            <div class="banner-area text-center">	
-                <div class="container">
+    <div class="banner-area text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="banner-content-wrapper">
+                        <div class="banner-content">
+                            <h2>Tradutor/Detalhes</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Banner Area End -->
+<!-- Teacher Start -->
+<div class="teacher-details-area pt-150 pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-5 col-xs-12">
+                <div class="teacher-details-img">
+                    <img src="img/teacher/teacher-details.jpg" alt="teacher">
+                </div>
+            </div>
+            <div class="col-md-7 col-sm-7 col-xs-12">
+                <div class="teacher-details-content ml-50">
+                    <h2>Samo Amós Fumo</h2>
+                    <h5>Tradutor de Inglês-Português</h5>
+                    <h4>Perfil</h4>
+                    <p>Serviços: tradução de todo o tipo de documentos, interpretação, revisão linguística de textos, transcrição de áudios, dublagem e legendagem de vídeos </p>
+                    <ul>
+                        <li><span>Grau: </span>Licenciado</li>
+                        <li><span>Experiência: </span>10 Aanos</li>
+                        <!-- <li><span>hobbies: </span>music, travelling, catching fish</li>
+                                <li><span>Faculdade: </span>Din, Department of Micro Biology</li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-4">
+                <div class="teacher-contact">
+                    <h4>Contactos</h4>
+                    <p><span>E-mail : </span>samotraducoes@gmail.com <br> samoamos@outlook.com</p>
+                    <p><span>Contactos : </span>(+258) 842214301</p>
+                    <p><span>Linkedln : </span> Samo Amós Fumo</p>
+                    <ul>
+                        <li><a href="https://https://l.wl.co/l?u=https%3A%2F%2Ffacebook.com%2Fsamotradutorajuramentado"><i class="zmdi zmdi-facebook"></i></a></li>
+                        <li><a href="https://www.wa.me/+258875078033"><i class="zmdi zmdi-whatsapp"></i></a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9 col-sm-8">
+                <div class="skill-area">
+                    <h4>Habilidades</h4>
+                </div>
+                <div class="skill-wrapper">
                     <div class="row">
-                        <div class="col-xs-12">
-                            <div class="banner-content-wrapper">
-                                <div class="banner-content">
-                                    <h2>Tradutor/Detalhes</h2> 
-                                </div> 
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Idiomas</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">85%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Trabalho em equipe</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 90%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">90%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Dinamismo</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">85%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Criatividade</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 95%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">95%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Inovador</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">85%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="skill-bar-item">
+                                <span>Comunicação</span>
+                                <div class="progress">
+                                    <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 95%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
+                                        <span class="text-top">95%</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>    
-		<!-- Banner Area End -->
-        <!-- Teacher Start -->
-        <div class="teacher-details-area pt-150 pb-60">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-5 col-sm-5 col-xs-12">
-                        <div class="teacher-details-img">
-                            <img src="img/teacher/teacher-details.jpg" alt="teacher">  
-                        </div>
-                    </div>
-                    <div class="col-md-7 col-sm-7 col-xs-12">
-                        <div class="teacher-details-content ml-50">
-                            <h2>Samo Amós Fumo</h2>
-                            <h5>Tradutor de Inglês-Português</h5>
-                            <h4>Perfil</h4>
-                            <p>Serviços: tradução de todo o tipo de documentos, interpretação, revisão linguística de textos, transcrição de áudios, dublagem e legendagem de vídeos </p>
-                            <ul>
-                                <li><span>Grau: </span>Licenciado</li>
-                                <li><span>Experiência: </span>10 Aanos</li>
-                                <!-- <li><span>hobbies: </span>music, travelling, catching fish</li>
-                                <li><span>Faculdade: </span>Din, Department of Micro Biology</li> -->
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3 col-sm-4">
-                        <div class="teacher-contact">
-                            <h4>Contactos</h4>
-                            <p><span>E-mail : </span>samotraducoes@gmail.com <br> samoamos@outlook.com</p>
-                            <p><span>Contactos : </span>(+258) 842214301</p>
-                            <p><span>Linkedln : </span> Samo Amós Fumo</p>
-                            <ul>
-                                <li><a href="https://https://l.wl.co/l?u=https%3A%2F%2Ffacebook.com%2Fsamotradutorajuramentado"><i class="zmdi zmdi-facebook"></i></a></li>
-                                <li><a href="https://www.wa.me/+258875078033"><i class="zmdi zmdi-whatsapp"></i></a></li>
-                                
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-sm-8">
-                        <div class="skill-area">
-                            <h4>Habilidades</h4>
-                        </div>  
-                        <div class="skill-wrapper">     
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Idiomas</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">85%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Trabalho em equipe</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 90%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">90%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Dinamismo</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">85%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Criatividade</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 95%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">95%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Inovador</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 85%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">85%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="skill-bar-item">
-                                        <span>Comunicação</span>
-                                        <div class="progress">
-                                            <div data-wow-delay="1.2s" data-wow-duration="1.5s" style="width: 95%; visibility: visible; animation-duration: 1.5s; animation-delay: 1.2s; animation-name: fadeInLeft;" data-progress="50%" class="progress-bar wow fadeInLeft">
-                                                <span class="text-top">95%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>   
-                        </div>     
-                    </div>
-                </div>
-            </div>
         </div>
-        <!-- Teacher End -->
+    </div>
+</div>
+<!-- Teacher End -->
 
 @endsection
