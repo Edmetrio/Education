f1d2ffb20ca575a8677015130bdb4a726dbe0bb3
Edmétrio J <edmetrio06@gmail.com>
2022-03-17 16:02:25 +0200

    academica relarions

--- app/Models/Models/Academica.php
+++ app/Models/Models/Academica.php
@@ -5,6 +5,7 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Academica extends Model
 {
@@ -15,7 +16,15 @@ class Academica extends Model
     protected $guarded = [];
 
     protected $table = 'academica';
-    protected $fillable = ['pessoa_id','escola','termino','certificado','estado'];
+    protected $fillable = ['pessoa_id','escola','termino','pais_id','certificado','estado'];
 
-    
+    public function exames()
+    {
+        return $this->belongsToMany(Exame::class, 'itemexame');
+    }
+
+    public function pessoas()
+    {
+        return $this->belongsTo(Pessoa::class, 'pessoa_id', 'id');
+    }
 }
