74aee6b48ca84c4db4a546d711b53bacc4337020
Edmétrio J <edmetrio06@gmail.com>
2022-03-21 10:29:44 +0200

    livewire inscricao

--- app/Http/Livewire/Inscricaos.php
+++ app/Http/Livewire/Inscricaos.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Models\Inscricao;
 use App\Models\Models\Intake;
+use App\Models\Models\Itemintake;
 use App\Models\Models\Pais;
 use App\Models\Servico;
 use Illuminate\Support\Facades\File;
@@ -13,40 +14,79 @@ use Livewire\WithFileUploads;
 class Inscricaos extends Component
 {
     use WithFileUploads;
-    public $user_id, $pais_id, $universidade, $abertura, $fecho, $link, $anexo;
+    public $user_id, $pais_id, $universidade, $abertura, $fecho, $link, $anexo, $intake_id;
     public $edit_id, $edit_user_id, $edit_pais_id, $edit_pais_nome, $edit_universidade, $edit_abertura, $edit_fecho, $edit_link, $edit_anexo, $old_anexo, $new_anexo;
+
+    public $pais;
+    public $universidades;
+    public $selectedPais = null;
+    public $selectedUniversidade = null;
 
     public $showData = true;
     public $createData = false;
     public $updateData = false;
 
+    public $tabela = true;
+
+    public $inputs = [];
+    public $i = 1;
+
+    public $search = '';
+
+    public function add($i)
+    {
+        $i = $i + 1;
+        $this->i = $i;
+        array_push($this->inputs, $i);
+    }
+
+    public function remove($i)
+    {
+        unset($this->inputs[$i]);
+    }
+
     private function resetInput()
     {
         $this->user_id = '';
         $this->pais_id = '';
+        $this->intake_id = '';
         $this->universidade = '';
         $this->abertura = '';
         $this->link = '';
         $this->fecho = '';
         $this->anexo = '';
     }
-
+  
     public function store()
     {
-        $validateDate = $this->validate([
-            'pais_id' => 'required',
-            'universidade' => 'required',
-            'abertura' => 'required',
-            'fecho' => 'required',
-            'link' => 'required',
-            'anexo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-        ]);
-
+        $validateDate = $this->validate(
+            [
+                'pais_id' => 'required',
+                'universidade' => 'required',
+                'abertura' => 'required',
+                'fecho' => 'required',
+                'link' => 'required',
+                'anexo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'intake_id.0' => 'required',
+                'intake_id.*' => 'required',
+            ],
+            [
+                'intake_id.0.required' => 'Seleccione o Intake',
+                'intake_id.*.required' => 'Seleccione o Intake',
+            ]
+        );
         $validateDate['anexo'] = $this->anexo->store('files', 'public');
         $validateDate['users_id'] = '474569f2-260c-46fc-833e-9d155f3894f2';
-        Inscricao::create($validateDate);
+        $inscricao = Inscricao::create($validateDate);
+        foreach ($this->intake_id as $key => $value)
+        {
+            Itemintake::create([
+                'inscricao_id' => $inscricao->id,
+                'intake_id' => $this->intake_id[$key]
+            ]);
+        }
         $this->resetInput();
-        session()->flash('status', 'Universidade criada com sucesso!'); 
+        session()->flash('status', 'Universidade criada com sucesso!');
     }
 
     public function edit($id)
@@ -78,13 +118,12 @@ class Inscricaos extends Component
         ]);
 
         $inscricao = Inscricao::findOrFail($id);
-        $destination = public_path('storage\\'.$inscricao->anexo);
+        $destination = public_path('storage\\' . $inscricao->anexo);
         $filename = "";
         if ($this->new_anexo === null) {
             $filename = $this->old_anexo;
         } else {
-            if(File::exists($destination))
-            {
+            if (File::exists($destination)) {
                 File::delete($destination);
             }
             $filename = $this->new_anexo->store('files', 'public');
@@ -103,13 +142,13 @@ class Inscricaos extends Component
         $this->createData = false;
         $this->updateData = false;
         $this->resetInput();
-        session()->flash('status', 'Universidade actualizada com sucesso!'); 
+        session()->flash('status', 'Universidade actualizada com sucesso!');
     }
 
     public function delete($id)
     {
         $inscricao = Inscricao::findOrFail($id);
-        $destination = public_path('storage\\'.$inscricao->anexo);
+        $destination = public_path('storage\\' . $inscricao->anexo);
         File::delete($destination);
         $inscricao->delete();
         $this->showData = true;
@@ -121,12 +160,37 @@ class Inscricaos extends Component
     public function mount()
     {
         $this->pais = Pais::orderBy('created_at', 'desc')->get();
-        $this->inscricao = Inscricao::with('pais')->orderBy('created_at', 'desc')->get();
+        $this->intake = Intake::orderBy('created_at', 'desc')->get();
     }
 
     public function render()
     {
+        $inscricao = Inscricao::with('pais','intakes','graus')->where('universidade', 'like', '%'.$this->search.'%')->orderBy('created_at', 'desc')->get();
+        $universidade = Inscricao::with('pais','intakes','graus')
+        ->where('universidade', 'like', '%'.$this->search.'%')->get();
+        /* dd($universidade); */
+        
         $sr = Servico::orderBy('created_at', 'desc')->get();
-        return view('livewire.inscricaos')->layout('layouts.app', compact('sr'));
+        return view('livewire.inscricaos', compact('inscricao','universidade'))->layout('layouts.app', compact('sr'));
     }
+
+    public function updatedSelectedPais($pais_id)
+    {
+        if(!is_null($pais_id))
+        {
+            $this->universidades = Inscricao::with('intakes','itemcursos.entidades')
+            ->where('universidade', 'like', '%'.$this->search.'%')->where('pais_id', $pais_id)
+            ->orderBy('created_at', 'desc')->get();
+            /* dd($this->universidades); */
+            /* $this->showData = false; */
+        }
+    }
+
+    /* public function updatedUniversidade($universidade_id)
+    {
+        if(!is_null($universidade_id))
+        {
+            $this->filtro = Inscricao::where('pais_id', $universidade_id)->get();
+        }
+    } */
 }
