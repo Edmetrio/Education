33b5ffadb885fc7f5473bd6d0b01a877150ec0a5
Edmétrio J <edmetrio06@gmail.com>
2022-03-21 10:31:29 +0200

    relation superior

--- app/Models/Models/Superior.php
+++ app/Models/Models/Superior.php
@@ -16,4 +16,39 @@ class Superior extends Model
 
     protected $table = 'superior';
     protected $fillable = ['pessoa_id','grau_id','pacote_id','moeda_id','intake_id','curso','orcamento'];
+
+    public function pessoas()
+    {
+        return $this->belongsTo(Pessoa::class, 'pessoa_id', 'id');
+    }
+
+    public function graus()
+    {
+        return $this->belongsTo(Grau::class, 'grau_id', 'id');
+    }
+
+    public function pacotes()
+    {
+        return $this->belongsTo(Pacote::class, 'pacote_id', 'id');
+    }
+
+    public function moedas()
+    {
+        return $this->belongsTo(Moeda::class, 'moeda_id', 'id');
+    }
+
+    public function intakes()
+    {
+        return $this->belongsTo(Intake::class, 'intake_id', 'id');
+    }
+
+    public function idiomas()
+    {
+        return $this->belongsToMany(Idioma::class, 'itemidioma');
+    }
+
+    public function pais()
+    {
+        return $this->belongsToMany(Pais::class, 'itempais');
+    }
 }
