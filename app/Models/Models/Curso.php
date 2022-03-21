7675c17e9d20c9ac236ebca9d46f095156595a63
Edmétrio J <edmetrio06@gmail.com>
2022-03-21 10:30:38 +0200

    model curso

--- app/Models/Models/Curso.php
+++ app/Models/Models/Curso.php
@@ -5,6 +5,7 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Curso extends Model
 {
@@ -17,13 +18,23 @@ class Curso extends Model
     protected $table = 'curso';
     protected $fillable = ['nome','categoria_id','estado'];
 
-    public function categorias()
+    /* public function categorias()
     {
         return $this->hasOne(Categoria::class, 'id', 'categoria_id');
-    }
+    } */
     
     public function itemcursos()
     {
         return $this->hasMany(Itemcurso::class, 'curso_id');
+    }
+
+    public function inscricaos()
+    {
+        return $this->belongsToMany(Inscricao::class, 'itemcurso');
+    }
+
+    public function categorias()
+    {
+        return $this->belongsTo(Categoria::class, 'categoria_id', 'id');
     }
 }
