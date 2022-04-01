a9571a7226e3e93acfec2c9885fd70d41833e36b
Edmétrio J <edmetrio06@gmail.com>
2022-04-01 16:33:02 +0200

    table comentario

---
+++ database/migrations/2022_04_01_123719_create_comentarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentario', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('users_id')->nullable();
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('poosts_id')->nullable();
+            $table->foreign('poosts_id')->references('id')->on('poost')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('nome')->nullable();
+            $table->string('curtir')->default('off')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentario');
+    }
+}
