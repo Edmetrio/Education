3374eddc549481283074e0647ccb67b6303c64f3
Edmétrio J <edmetrio06@gmail.com>
2022-04-04 11:10:55 +0200

    auth

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('cadastro', [RegisteredUserController::class, 'create'])
+    Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('cadastro', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('entrar', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
