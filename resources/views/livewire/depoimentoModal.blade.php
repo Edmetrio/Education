15a0f40d12a61baea6c4be23f494e8e6e8401eff
Edmétrio J <edmetrio06@gmail.com>
2022-04-06 12:27:03 +0200

    depoimentomodal resource

---
+++ resources/views/livewire/depoimentoModal.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="addDepoimento" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="staticBackdropLabel" >Aceitaçao</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            <h3 class="mb-3">Depoimento da  </h3>
+            <div class="col-md-11 col-sm-12">
+                <div class="about-img">
+                    <a href="{{asset('img/site/Aceitacao.jpg')}}"> <img src="{{asset('img/site/Aceitacao.jpg')}}" alt="about"></a>
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Descordo</button> -->
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>
+            </div>
+        </div>
+    </div>
+</div>
