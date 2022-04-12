186c8339c18dda3c0b8e32cc711542ef81c32f97
Edmétrio J <edmetrio06@gmail.com>
2022-04-12 10:02:19 +0200

    livewire users

---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,111 @@
+<div>
+    <div class="banner-area-wrapper">
+        <div class="banner-area text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="banner-content-wrapper">
+                            <div class="banner-content">
+                                <h2>Gerir Utilizadores</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="courses-details-area blog-area pt-100 pb-20">
+        <div class="container">
+            @if ($message = Session::get('status'))
+            <div>
+                <p class="alert alert-success" class="table p-field p-col-12 p-md-6 table-striped" style="text-align: center;">{{ $message }}</p>
+            </div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">E-mail</th>
+                                    <th scope="col">Roles</th>
+                                    <th scope="col">Criação</th>
+                                    <th scope="col">Acções</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $s)
+                                <tr>
+                                    <td>{{ $s->name }}</td>
+                                    <td>{{ $s->email }}</td>
+                                    <td>{{ $s->roles->nome }}</td>
+                                    <td>{{ $s->created_at->diffForhumans() }}</td>
+                                    <td>
+                                        <button wire:click="edit('{{$s->id}}')"><i class="fas fa-edit"></i></button>
+                                        <button wire:click="delete('{{ $s->id }}')"><i class="fas fa-trash"></button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                @if($updateData == true)
+                <div class="col-md-8">
+                    <div class="courses-details">
+                        <div class="course-details-content">
+                            <h2>Utilizador</h2>
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <strong>Opss!</strong> Algum problema com seu formulário<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <hr>
+                            <form>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Nome:</strong>
+                                            <select class="form-control" wire:model="users_id">
+                                                <option>Seleccione o Serviço</option>
+                                                @foreach($users as $s)
+                                                <option value="{{ $s->id }}">{{ $s->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('users_id') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Roles:</strong>
+                                            <select class="form-control" wire:model="role_id">
+                                                <option>Seleccione o Roles</option>
+                                                @foreach($role as $r)
+                                                <option value="{{ $r->id }}">{{ $r->nome }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('role_id') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <button type="submit" wire:click.prevent="update()" class="btn btn-primary">Alterar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endif
+
+            </div>
+        </div>
+    </div>
+</div>
