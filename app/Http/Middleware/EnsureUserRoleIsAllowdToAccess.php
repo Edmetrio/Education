68e30f2d7563c8a2a9988c26922159be862491bd
Edmétrio J <edmetrio06@gmail.com>
2022-04-13 11:54:29 +0200

    ensure user role

---
+++ app/Http/Middleware/EnsureUserRoleIsAllowdToAccess.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Models\Permissao;
+use App\Models\Models\Role;
+use App\Models\Models\Rota;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class EnsureUserRoleIsAllowdToAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        /* echo 'middleware. <br/>'; */
+
+        $user = Auth::user()->role_id;
+        $route = Route::currentRouteName();
+        $rota = Rota::where('nome', $route)->first();
+        /* echo $user.'<br/>';
+        echo $route.'<br/>'; */
+
+        try {
+            if (Permissao::isRoleHasRightToAccess($user, $rota->id)
+                || in_array($route, $this->defaultUserAccessRole()[$user])) {
+                return $next($request);
+            } else {
+                abort(403, 'Não Autorizado!');
+            }
+        } catch (\Throwable $th) {
+            abort(403, 'Você não esta autorizador a aceder essa página!');
+        }
+
+        
+    }
+
+    private function defaultUserAccessRole()
+    {
+        $admin = Role::with('rotas')->where('nome', 'admin')->first();
+        return [
+            $admin->id => [
+                'users',
+                'permissaos'
+            ]
+        ];
+    }
+}
