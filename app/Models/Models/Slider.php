995b1ca9f4e2038bf17c811534f6859748ee9b2f
Edmétrio J <edmetrio06@gmail.com>
2022-04-13 11:55:05 +0200

    model slider

--- app/Models/Models/Slider.php
+++ app/Models/Models/Slider.php
@@ -15,5 +15,5 @@ class Slider extends Model
     protected $guarded = [];
 
     protected $table = 'slider';
-    protected $fillable = ['nome','icon','descricao','texto','estado'];
+    protected $fillable = ['nome','icon','descricao','texto','texto1','texto2','link','estado'];
 }
