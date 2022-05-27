06949614243188c30d0625657bebd0330ddf49c6
Edmétrio J <edmetrio06@gmail.com>
2022-05-27 13:01:59 +0200

    inicio controller

--- app/Http/Livewire/Inicios.php
+++ app/Http/Livewire/Inicios.php
@@ -25,11 +25,11 @@ class Inicios extends Component
     public function render()
     {
         $poost = Poost::orderBy('created_at', 'desc')->paginate(4);
-        $servico = Servico::orderBy('created_at', 'asc')->get();
+        $servico = Servico::orderBy('created_at', 'desc')->get();
         $sr = Servico::orderBy('created_at', 'desc')->get();
         $depoimento = Depoimento::orderBy('created_at', 'desc')->paginate(6);
         $parceiro = Parceiro::orderBy('created_at', 'desc')->paginate(4);
-        $slider = Slider::orderBy('created_at', 'asc')->paginate(6);
+        $slider = Slider::orderBy('created_at', 'desc')->paginate(6);
         return view('livewire.inicios', compact('poost', 'parceiro', 'depoimento', 'servico'))->layout('layouts.template', compact('slider', 'sr'));
     }
 }
