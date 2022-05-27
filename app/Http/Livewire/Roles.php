9b133b8b1d41861eef523c0762eb34c4bd32d4bd
Edmétrio J <edmetrio06@gmail.com>
2022-05-27 13:02:20 +0200

    controller roles

---
+++ app/Http/Livewire/Roles.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Models\Role;
+use App\Models\Servico;
+use Livewire\Component;
+
+class Roles extends Component
+{
+    public $nome, $edit_nome, $edit_id;
+    public $createData  = true, $updateData = false, $showData = true;
+
+    private function resetInput()
+    {
+        $this->nome = '';
+    }
+
+    public function store()
+    {
+        $validateDate = $this->validate([
+            'nome' => 'required',
+        ]);
+
+        Role::create($validateDate);
+        $this->resetInput();
+        session()->flash('status', 'Role criado com sucesso!'); 
+    }
+
+    public function edit($id)
+    {
+        $this->updateData = true;
+        $this->createData = false;
+        $role = Role::findOrFail($id);
+        $this->edit_nome = $role->nome;
+        $this->edit_id = $id;
+    }
+
+    public function update()
+    {
+        $validateDate = $this->validate([
+            'edit_nome' => 'required',
+        ]);
+
+        $validateDate['nome'] = $this->edit_nome;
+        Role::findOrfail($this->edit_id)->update($validateDate);
+        $this->resetInput();
+        $this->updateData = false;
+        $this->createData = true;
+        session()->flash('status', 'Role actualizado com sucesso!'); 
+    }
+
+    public function delete($id)
+    {
+        Role::findOrFail($id)->delete();
+        $this->resetInput();
+        session()->flash('status', 'Role apagado com sucesso!'); 
+    }
+
+    public function render()
+    {
+        $this->roles = Role::orderBy('created_at', 'desc')->get();
+        $sr = Servico::orderBy('created_at', 'desc')->get();
+        return view('livewire.roles')->layout('layouts.appp', compact('sr'));
+    }
+}
