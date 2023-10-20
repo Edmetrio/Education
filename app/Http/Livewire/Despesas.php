e46bb191bd644ae6a5e2c93e5dbe564fa2ddf41f
Edmétrio J <edmetrio06@gmail.com>
2023-10-20 11:43:34 +0200

    Livewire candidato, remanescente e cotação

--- app/Http/Livewire/Despesas.php
+++ app/Http/Livewire/Despesas.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Models\Candidato;
+use App\Models\Models\Cotacao;
 use App\Models\Models\Despesas as ModelsDespesas;
+use App\Models\Models\Remanescente;
 use App\Models\Models\Subcategoria;
 use App\Models\Models\Tipodespesa;
 use App\Models\Servico;
@@ -14,9 +17,13 @@ use Livewire\WithPagination;
 class Despesas extends Component
 {
     public $subcategoria_id, $user_id, $nome, $descricao, $valor, $despesa_id, $nomem, $valorm, $inicio, $fim, $st, $nomet, $tipo_id;
+    public $cotacao_id;
     public $updateMode = false;
     public $tabela = false;
+    public $updateCategoria = false;
     public $pesquisa = '';
+    /* public $categoria = ''; */
+    public $selectedState = NULL;
     use WithPagination;
 
     private function resetInputFields()
@@ -26,6 +33,7 @@ class Despesas extends Component
         $this->descricao = '';
         $this->valor = '';
         $this->tipo_id = '';
+        $this->cotacao_id = '';
     }
 
     private function resetInputFieldsModal()
@@ -38,16 +46,24 @@ class Despesas extends Component
     public function store()
     {
         $validateDate = $this->validate([
-            'subcategoria_id' => 'required',
+            /* 'state' => 'required', */
             'nome' => 'required',
             'descricao' => 'required',
             'valor' => 'required',
             'tipo_id' => 'required'
         ]);
-        
+
+        $validateDate['subcategoria_id'] = $this->selectedState;
         $validateDate['tipodespesa_id'] = $this->tipo_id;
         $validateDate['user_id'] = Auth::user()->id;
-        ModelsDespesas::create($validateDate);
+        if ($this->selectedState === 'aa2870b1-8ea9-42c8-9f80-0a7d3c5e90d2') {
+            $validateDate['cotacao_id'] = $this->cotacao_id;
+            ModelsDespesas::create($validateDate);
+            Remanescente::create($validateDate);
+        } else {
+            ModelsDespesas::create($validateDate);
+        }
+        
 
         $this->message = 'Despesa criada com sucesso!';
         $this->text = 'Por favor, Verifique a tabela das despesas';
@@ -152,6 +168,17 @@ class Despesas extends Component
         $this->emit('tipoAdded');
     }
 
+    public function updatedSelectedState($state)
+    {
+        if ($state === 'aa2870b1-8ea9-42c8-9f80-0a7d3c5e90d2') {
+            $this->updateCategoria = true;
+        } else {
+            $this->updateCategoria = false;
+        }
+        /* $despesas = ModelsDespesas::where('subcategoria_id', $state)->get();
+        dd($despesas); */
+    }
+
     /* public function mount()
     {
         $this->user = User::orderBy('created_at', 'desc')->get();
@@ -161,14 +188,16 @@ class Despesas extends Component
     {
         $sr = Servico::orderBy('created_at', 'asc')->get();
         $total = 0.0;
-        $despesa = ModelsDespesas::with('subcategorias','users')->where('nome', 'like', '%'. $this->pesquisa . '%')->orderBy('created_at', 'desc')->paginate(10);
+        $despesa = ModelsDespesas::with('subcategorias','users')->where('nome', 'like', '%'. $this->pesquisa . '%')->orderBy('created_at', 'desc')->get();
         foreach($despesa as $d) {
             $total += $d->valor;
         }
         $this->dp = $total;
         $categoria = Subcategoria::orderBy('created_at', 'desc')->get();
+        $cotacao = Cotacao::orderBy('created_at', 'desc')->get();
+        $estudante = Candidato::orderBy('created_at', 'desc')->get();
         $tipo = Tipodespesa::orderBy('created_at', 'desc')->get();
-        return view('livewire.despesas', compact('despesa','categoria', 'tipo'))->layout('layouts.appp', compact('sr'));
+        return view('livewire.despesas', compact('despesa','categoria', 'tipo','cotacao','estudante'))->layout('layouts.appp', compact('sr'));
     }
 
     public function alertSuccess()
